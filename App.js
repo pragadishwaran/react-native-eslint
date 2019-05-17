5087431624982dfed614e01642f0712a45e5c99d
pragadishwaran <pragadishwaran.baskaran@cesltd.com>
2019-05-17 17:25:32 +0530

    initcommit

---
+++ App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import configureStore from './src/store';
+// import FirstComponent from './src/components/FirstComponent';
+import { LoginScreen } from './src/Screens';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={configureStore}>
+      <LoginScreen />
+      </Provider>
+    );
+  }
+}
+
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,10 @@
+import React from  'react';
+import { View } from 'react-native';
+
+export default class Layout extends React.Component  {
+    render() {
+        return(
+            <View>{this.props.children}</View>
+        )
+    }
+}
